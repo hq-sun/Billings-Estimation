df2592d74ca3d80a41067717369cceaf53d7723e
hq-sun <38084894+hq-sun@users.noreply.github.com>
2020-06-20 13:47:51 -0400

    Add LSTM

--- YipitData Challenge.py
+++ YipitData Challenge.py
@@ -161,13 +161,19 @@ local_ak = local_agg_full_sorted.interpolate(method='akima') ##  smooth plotting
 local_ak.plot()
 ## 436
 
-# Method 5 - MICE OKAY
+# Method 5 - time Interpolation
+# Works on daily and higher resolution data to interpolate given length of interval
+local_time = local_agg_full_sorted.interpolate(method='time')
+local_time.plot()
+
+
+# Method 6 - MICE OKAY
 local_mice = pd.read_csv(r'./data/clean/local_mice.csv')
 local_mice.set_index('Start.Date', inplace=True)
 local_mice.plot()
 ## 417
 
-# Method 6 - kNN -- not so good b/c all missing days are imputed as same
+# Method 7 - kNN -- not so good b/c all missing days are imputed as same
 from sklearn.impute import KNNImputer
 local_knn = KNNImputer(n_neighbors=5).fit_transform(local_agg_full_sorted)
 local_knn = pd.DataFrame(local_knn)
@@ -182,6 +188,10 @@ local_ak.sum(axis = 0, skipna = True)/1000000
 # Units Sold       14.432131
 # Billings        436.451398
 
+local_time.sum(axis = 0, skipna = True)/1000000
+# Units Sold       14.992623
+# Billings        442.018623
+
 goods_agg.sum(axis = 0, skipna = True)/1000000
 # Units Sold       10.419746
 # Billings        282.245671
@@ -194,14 +204,97 @@ travel_agg.sum(axis = 0, skipna = True)/1000000
 # =============================================================================
 # LSTM
 # =============================================================================
+import math
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
 
+# Split local dataframe to two - Yeear 2012 and Year 2013 (omit one obs in 2011)
+local_2013_full = local_agg_full_sorted.loc['2013-01-01':'2013-12-31'] ## 365 obs
+local_2012 = local_agg_full_sorted.loc['2012-01-01':'2012-12-31'] ## 149 obs (after Sep 1, no missing)
+local_2013_full.plot()
+local_2012.plot()
+
+local_2013_9_12 = local_agg_full_sorted.loc['2013-09-01':'2013-12-31'] 
+local_2012_9_12 = local_agg_full_sorted.loc['2012-09-01':'2012-12-31'] 
+local_2013_9_12.plot()
+local_2012_9_12.plot()
+
+local_2013_9_12_ts = local_2013_9_12[['Billings']]
+local_2012_9_12_ts = local_2012_9_12[['Billings']]
 
 
+# fix random seed for reproducibility
+np.random.seed(7)
+
+dataset = local_2013_9_12_ts.values
+dataset = dataset.astype('float32')
+
+# normalize the dataset
+scaler = MinMaxScaler(feature_range=(0, 1))
+dataset = scaler.fit_transform(dataset)
+
+# split into train and test sets
+train_size = int(len(dataset) * 0.67)
+test_size = len(dataset) - train_size
+train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+print(len(train), len(test))
+
+
+# convert an array of values into a dataset matrix
+def create_dataset(dataset, look_back=1):
+	dataX, dataY = [], []
+	for i in range(len(dataset)-look_back-1):
+		a = dataset[i:(i+look_back), 0]
+		dataX.append(a)
+		dataY.append(dataset[i + look_back, 0])
+	return np.array(dataX), np.array(dataY)
+
+# reshape into X=t and Y=t+1
+look_back = 1
+trainX, trainY = create_dataset(train, look_back)
+testX, testY = create_dataset(test, look_back)
+
+# reshape input to be [samples, time steps, features]
+trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
+testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+
+# create and fit the LSTM network
+model = Sequential()
+model.add(LSTM(4, input_shape=(1, look_back)))
+model.add(Dense(1))
+model.compile(loss='mean_squared_error', optimizer='adam')
+model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=2)
+
+# make predictions
+trainPredict = model.predict(trainX)
+testPredict = model.predict(testX)
+# invert predictions
+trainPredict = scaler.inverse_transform(trainPredict)
+trainY = scaler.inverse_transform([trainY])
+testPredict = scaler.inverse_transform(testPredict)
+testY = scaler.inverse_transform([testY])
+# calculate root mean squared error
+trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
+print('Train Score: %.2f RMSE' % (trainScore))
+testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+print('Test Score: %.2f RMSE' % (testScore))
+
+# shift train predictions for plotting
+trainPredictPlot = np.empty_like(dataset)
+trainPredictPlot[:, :] = np.nan
+trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+# shift test predictions for plotting
+testPredictPlot = np.empty_like(dataset)
+testPredictPlot[:, :] = np.nan
+testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
+# plot baseline and predictions
+plt.plot(scaler.inverse_transform(dataset))
+plt.plot(trainPredictPlot)
+plt.plot(testPredictPlot)
+plt.show()
 
 # =============================================================================
 # Try Some TIme Series
