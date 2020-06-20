6fcf185d1eefd7d932fe14b01a3424766d4a3659
hq-sun <38084894+hq-sun@users.noreply.github.com>
2020-06-20 14:23:46 -0400

    Update YipitData Challenge.py

--- YipitData Challenge.py
+++ YipitData Challenge.py
@@ -201,15 +201,6 @@ travel_agg.sum(axis = 0, skipna = True)/1000000
 # Billings        70.552062
 
 
-# =============================================================================
-# LSTM
-# =============================================================================
-import math
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import mean_squared_error
 
 # Split local dataframe to two - Yeear 2012 and Year 2013 (omit one obs in 2011)
 local_2013_full = local_agg_full_sorted.loc['2013-01-01':'2013-12-31'] ## 365 obs
@@ -222,113 +213,21 @@ local_2012_9_12 = local_agg_full_sorted.loc['2012-09-01':'2012-12-31']
 local_2013_9_12.plot()
 local_2012_9_12.plot()
 
+
 local_2013_9_12_ts = local_2013_9_12[['Billings']]
 local_2012_9_12_ts = local_2012_9_12[['Billings']]
 
+df1 = local_2013_9_12_ts.copy()
+df2 = local_2012_9_12_ts.copy()
 
-# fix random seed for reproducibility
-np.random.seed(7)
+df1.reset_index(drop=True, inplace=True)
+df2.reset_index(drop=True, inplace=True)
 
-dataset = local_2013_9_12_ts.values
-dataset = dataset.astype('float32')
+df = pd.concat( [df1, df2], axis=1) 
+df.columns = ['a', 'b']
 
-# normalize the dataset
-scaler = MinMaxScaler(feature_range=(0, 1))
-dataset = scaler.fit_transform(dataset)
+df.plot()
 
-# split into train and test sets
-train_size = int(len(dataset) * 0.67)
-test_size = len(dataset) - train_size
-train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
-print(len(train), len(test))
-
-
-# convert an array of values into a dataset matrix
-def create_dataset(dataset, look_back=1):
-	dataX, dataY = [], []
-	for i in range(len(dataset)-look_back-1):
-		a = dataset[i:(i+look_back), 0]
-		dataX.append(a)
-		dataY.append(dataset[i + look_back, 0])
-	return np.array(dataX), np.array(dataY)
-
-# reshape into X=t and Y=t+1
-look_back = 1
-trainX, trainY = create_dataset(train, look_back)
-testX, testY = create_dataset(test, look_back)
-
-# reshape input to be [samples, time steps, features]
-trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
-testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
-
-# create and fit the LSTM network
-model = Sequential()
-model.add(LSTM(4, input_shape=(1, look_back)))
-model.add(Dense(1))
-model.compile(loss='mean_squared_error', optimizer='adam')
-model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=2)
-
-# make predictions
-trainPredict = model.predict(trainX)
-testPredict = model.predict(testX)
-# invert predictions
-trainPredict = scaler.inverse_transform(trainPredict)
-trainY = scaler.inverse_transform([trainY])
-testPredict = scaler.inverse_transform(testPredict)
-testY = scaler.inverse_transform([testY])
-# calculate root mean squared error
-trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
-print('Train Score: %.2f RMSE' % (trainScore))
-testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
-print('Test Score: %.2f RMSE' % (testScore))
-
-# shift train predictions for plotting
-trainPredictPlot = np.empty_like(dataset)
-trainPredictPlot[:, :] = np.nan
-trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
-# shift test predictions for plotting
-testPredictPlot = np.empty_like(dataset)
-testPredictPlot[:, :] = np.nan
-testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
-# plot baseline and predictions
-plt.plot(scaler.inverse_transform(dataset))
-plt.plot(trainPredictPlot)
-plt.plot(testPredictPlot)
-plt.show()
-
-# =============================================================================
-# Try Some TIme Series
-# =============================================================================
-local_ts = local[['Start Date','Billings']]
-local_ts['Billings'].value_counts()
-local_ts['Billings'].isnull().sum()
-local_ts['Start Date'] = local_ts['Start Date'].astype(str)
-
-goods_ts = goods[['Start Date','Billings']]
-goods_ts['Billings'].value_counts()
-goods_ts['Billings'].isnull().sum()
-goods_ts['Start Date'] = goods_ts['Start Date'].astype(str)
-
-travel_ts = travel[['Start Date','Billings']]
-travel_ts['Billings'].value_counts()
-travel_ts['Billings'].isnull().sum()
-travel_ts['Start Date'] = travel_ts['Start Date'].astype(str)
-
-# Aggreagation to day level using sum
-local_ts_agg = local_ts.groupby(['Start Date'], as_index=True)['Billings'].sum()
-goods_ts_agg = goods_ts.groupby(['Start Date'], as_index=True)['Billings'].sum()
-travel_ts_agg = travel_ts.groupby(['Start Date'], as_index=True)['Billings'].sum()
-
-# Some plots
-from matplotlib import pyplot
-local_ts_agg.plot()
-pyplot.show()
-
-goods_ts_agg.plot()
-pyplot.show()
-
-travel_ts_agg.plot()
-pyplot.show()
 
 
 
